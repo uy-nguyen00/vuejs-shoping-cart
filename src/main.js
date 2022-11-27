7d17bc21cce09087b1f3aa63d34473af7f79a3c0
Uy N <cao.nguyen@etu.umontpellier.fr>
2022-11-27 20:25:09 +0100

    simple shoping cart app using vuejs - done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import router from './router.js';
+import store from "@/store/index";
+import App from './App.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+
+const app = createApp(App)
+
+app.use(router);
+app.use(store);
+
+app.component('base-badge', BaseBadge);
+
+app.mount('#app');
